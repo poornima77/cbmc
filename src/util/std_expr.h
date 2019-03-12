073dfd4e8469f4c278115943e5f475523957b22e
Daniel Kroening <kroening@cs.ox.ac.uk>
2019-03-12 13:54:09 +0000

    remove deprecated array_of_exprt constructor

--- src/util/std_expr.h
+++ src/util/std_expr.h
@@ -1575,11 +1575,6 @@ inline index_exprt &to_index_expr(exprt &expr)
 class array_of_exprt:public unary_exprt
 {
 public:
-  DEPRECATED("use array_of_exprt(what, type) instead")
-  array_of_exprt():unary_exprt(ID_array_of)
-  {
-  }
-
   explicit array_of_exprt(exprt _what, array_typet _type)
     : unary_exprt(ID_array_of, std::move(_what), std::move(_type))
   {
